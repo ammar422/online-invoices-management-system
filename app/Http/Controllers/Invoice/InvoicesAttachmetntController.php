8d39658dc46021d397be0441ff28a9b8f44c4a43
ammar <amarsamir632@gmail.com>
2024-03-21 20:56:51 +0200

    create invoice attachments(request+model+controller+migration DB table)

---
+++ app/Http/Controllers/Invoice/InvoicesAttachmetntController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Invoice;
+
+use App\Models\InvoicesAttachmetnt;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\invoice\StoreInvoicesAttachmetntRequest;
+use App\Http\Requests\invoice\UpdateInvoicesAttachmetntRequest;
+
+class InvoicesAttachmetntController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvoicesAttachmetntRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InvoicesAttachmetnt $invoicesAttachmetnt)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InvoicesAttachmetnt $invoicesAttachmetnt)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInvoicesAttachmetntRequest $request, InvoicesAttachmetnt $invoicesAttachmetnt)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InvoicesAttachmetnt $invoicesAttachmetnt)
+    {
+        //
+    }
+}
